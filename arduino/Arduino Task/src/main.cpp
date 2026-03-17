b886916db6b6504816b548e6b05a12252f8c704d
Alexander <aostropytskyy@gmail.com>
2026-03-17 10:34:10 +0000

    using main cpp using platformio to upload a test of the temperature code, also testing if new function works

--- arduino/Arduino Task/src/main.cpp
+++ arduino/Arduino Task/src/main.cpp
@@ -1,22 +1,39 @@
 #include <Arduino.h>
+// Loovee @ 2015-8-26
+#include <math.h>
+const int B = 4275000; // B value of the thermistor
+const int R0 = 100000; // R0 = 100k
+const int pinTempSensor = A0; // Grove - Temperature Sensor connect to A0
 
-// put function declarations here:
-int myFunction(int, int);
+int collect_temperature_data();
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-  Serial.println(result);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println("Hi!");
-  delay(2000);
+int collect_temperature_data()
+{
+  int temp_store[300];
+  for (int i =0; i<300;i++){
+    temp_store[i] = analogRead(pinTempSensor);
+  }
+  return 0;
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+  int x=0;
+  int a = analogRead(pinTempSensor);
+  float R = 1023.0/a-1.0;
+  R = R0*R;
+  float temperature = 1.0/(log(R/R0)/B+1/298.15)-273.15; // convert to temperature via datasheet
+  Serial.print("temperature = ");
+  Serial.println(temperature);
+  delay(1000);
+  collect_temperature_data();
+  x++;
+  if(x==300){
+    Serial.println(collect_temperature_data());
+  }
 }
